a591d7e8e524c3f77fe47ac1a11576b855b4b5fd
Manikarnika Jain <manikarnikajain31@gmail.com>
2022-06-17 20:13:44 +0530

    initial status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'datasharing';
+  currentBeverage = "coffee";
+  beverages = ['milk', 'tea', 'coffee', 'juice'];
+
+
+  addBeverage(newBeverage: string) {
+    this.beverages.push(newBeverage);
+    this.clearInput();
+  }
+
+  // Clears beverage on refreshing the page
+  clearInput() {
+    let input = <HTMLInputElement>document.getElementById('beverage-input');
+    input.value = '';
+  }
 }
